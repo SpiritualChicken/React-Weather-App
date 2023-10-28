6198f21ed33fa2feeb8e5be9f20e3fa2652b893b
Charlie <cnewlinds@gmail.com>
2023-10-28 16:13:31 +1100

    search cleanup

--- my-app/src/components/Search.jsx
+++ my-app/src/components/Search.jsx
@@ -4,32 +4,23 @@ import searchIcon from '../assets/search.png';
 
 
 function Search () {
-
-const [temp, setTemp] = useState(0);
-const [wind, setWind] = useState(0);
-const [humidity, setHumidity]= useState(0);
-const [weatherImg, setWeatherImg ] = useState("")
-const [isFave, setIsFave] = useState(false)
 const [ inputValue, setInputValue ] = useState("")
-const [searched, setSearched] =useState(false)
-const windSpeed = Math.floor(wind * 3.6)
-const displayTemp = Math.floor(temp)
+const [searchDisplay, setSearchDisplay] = useState(false)
 
 const handleChange = (event) => {
     setInputValue(event.target.value)
 } 
 const handleSubmit = (event) => {
     event.preventDefault(); 
+    setSearchDisplay(true);
+    
 
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=Metric&appid=cc2551d6f015c0c9fc75ac14012f75dc`)
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-            setTemp(data.main.temp)
-            setWind(data.wind.speed)
-            setHumidity(data.main.humidity)
-            setWeatherImg(data.weather[0].main.toLowerCase())
-          });
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=Metric&appid=cc2551d6f015c0c9fc75ac14012f75dc`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+
+        });
       
 }
 
