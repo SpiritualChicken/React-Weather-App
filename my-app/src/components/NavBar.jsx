cef02640db06a6bb173d3f1a5026bb62f1983c45
Charlie <cnewlinds@gmail.com>
2023-10-28 15:33:44 +1100

    add basic search functionality

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar () {
     return (
         <nav className="navBar"> 
             <NavLink exact to="/" className="navItem">Home</NavLink>
-            <NavLink to="/favourites" className="navItem">favourites</NavLink>
+            <NavLink to="/favourites" className="navItem">Favourites</NavLink>
         </nav>
     )
 }
--- my-app/src/components/Search.jsx
+++ my-app/src/components/Search.jsx
@@ -1,28 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import searchIcon from '../assets/search.png'; 
+
 
 
 function Search () {
 
-    const [ inputValue, setInputValue ] = useState("")
+const [temp, setTemp] = useState(0);
+const [wind, setWind] = useState(0);
+const [humidity, setHumidity]= useState(0);
+const [weatherImg, setWeatherImg ] = useState("")
+const [isFave, setIsFave] = useState(false)
+const [ inputValue, setInputValue ] = useState("")
+const [searched, setSearched] =useState(false)
+const windSpeed = Math.floor(wind * 3.6)
+const displayTemp = Math.floor(temp)
 
 const handleChange = (event) => {
     setInputValue(event.target.value)
 } 
 const handleSubmit = (event) => {
     event.preventDefault(); 
-    console.log(`${inputValue} was searched`);
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=Metric&appid=cc2551d6f015c0c9fc75ac14012f75dc`)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            setTemp(data.main.temp)
+            setWind(data.wind.speed)
+            setHumidity(data.main.humidity)
+            setWeatherImg(data.weather[0].main.toLowerCase())
+          });
+      
 }
 
-    const handleClick = () => { 
-        console.log(`${inputValue} was searched`)
-    }
 
     return (
     <form className="topBar" onSubmit={handleSubmit}>
         <input type="text" className="cityInput" placeholder="Search..." value={inputValue} onChange={handleChange} />
         <button className="searchIcon">
-            <img src={searchIcon} alt="Search" onClick={handleClick} />
+            <img src={searchIcon} alt="Search" />
         </button>
     </form>
     )
